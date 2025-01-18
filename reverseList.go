c5c9ebd43f90df670764d4cf881a771d5cc85cdd
wutianhao <553404185@qq.com>
2025-01-19 00:16:51 +0800

    add:反转链表

--- reverseList.go
+++ reverseList.go
@@ -1,16 +1,19 @@
 package main
 
 // 你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 双指针写法
 func reverseList(head *ListNode) *ListNode {
-	var node *ListNode
+	// 向前一位的指针
+	var preNode *ListNode
+	// 指针指向当前的节点
 	cur := head
 
 	for cur != nil {
-		temp := cur.Next // 第一步设定一个值把
-		cur.Next = node
-		node = cur
-		cur = temp
+		temp := cur.Next   // 第一步 先保留原来的指针情况，防止指针链接下一个元素断掉
+		cur.Next = preNode // 让cur的指针的下一个元素指向pre node节点
+		preNode = cur      // pre的节点指向当前的cur指针
+		cur = temp         // 让当前的cur往下继续移动
 	}
 
-	return node
+	return preNode
 }
